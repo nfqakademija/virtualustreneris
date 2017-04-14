73c415d2a78feacea1a5f09b0d34e3fcd855297c
Armandas Dambrauskas <armas_z@yahoo.com>
2017-04-14 22:10:30 +0300

    base.html , main.js, main.scss edited loader added

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame, 1);
+    
+    function frame() {
+        if(loading === 100) {
+            clearInterval(id);
+            document.getElementById("preload").style.display = "none";
+        } else {
+          loading = loading + 1;          
+          if(loading === 90) {
+              console.log("loading", loading);
+              preload.style.annimation = "fadeout 1s ease";
+          }
+        }
+    }
+    
 });
