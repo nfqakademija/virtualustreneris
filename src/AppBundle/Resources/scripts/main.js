26689aa5396a7411545749299b499472c01da686
Aidas Eglinskas <programeriss@gmail.com>
2017-04-16 11:09:39 +0300

    Updated POP-UP

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -19,12 +19,12 @@ $(document).ready(function(){
 
     //POPup
     var modal = document.getElementById('myModal');
-    var btn = document.getElementById("myBtn");
     var span = document.getElementsByClassName("close")[0];
 
-    btn.onclick = function() {
+    function btn() {
         modal.style.display = "block";
-    };
+
+    }
 
     span.onclick = function() {
         modal.style.display = "none";
