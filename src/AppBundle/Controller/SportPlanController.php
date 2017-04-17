1241ccbf59e0414c30130e2fe5fd5cfee0d8ead1
Vilius Norbutas <norbutasvilius@gmail.com>
2017-04-17 14:26:46 +0300

    SportPlanController added

---
+++ src/AppBundle/Controller/SportPlanController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Response;
+
+class MealPlanController extends Controller
+{
+
+    /**
+     * @Route("/mitybos-programos", name="meal-plan")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:MealPlan:index.html.twig');
+    }
+
+}
