5b37a680a590fed9b7678620550d28722094fff3
Aidas Eglinskas <programeriss@gmail.com>
2017-04-17 13:31:21 +0300

    Updated Meal-Page

--- src/AppBundle/Controller/MealPlanController.php
+++ src/AppBundle/Controller/MealPlanController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\BrowserKit\Response;
 
-class HomeController extends Controller
+class MealPlanController extends Controller
 {
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/mitybos-programos", name="meal-plan")
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:Home:index.html.twig');
+        return $this->render('AppBundle:MealPlan:index.html.twig');
     }
-    
+
 }
