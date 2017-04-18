28b77826cd37703f9f474d844dc6529f4ce3ed9a
Vilius Norbutas <norbutasvilius@gmail.com>
2017-04-18 14:20:15 +0300

    main controllers and views created

--- src/AppBundle/Controller/MealPlanController.php
+++ src/AppBundle/Controller/MealPlanController.php
@@ -17,4 +17,37 @@ class MealPlanController extends Controller
         return $this->render('AppBundle:MealPlan:index.html.twig');
     }
 
+
+    /**
+     * @Route("admin/mitybos-programos/list", name="meal-plan-list")
+     */
+    public function mealListAction()
+    {
+        return $this->render('AppBundle:MealPlan:list.html.twig');
+    }
+
+    /**
+     * @Route("admin/mitybos-programos/show/{id}", name="meal-plan-show")
+     */
+    public function mealShowAction($id)
+    {
+        return $this->render('AppBundle:MealPlan:show.html.twig');
+    }
+
+    /**
+     * @Route("admin/mitybos-programos/edit/{id}", name="meal-plan-edit")
+     */
+    public function mealEditAction($id)
+    {
+        return $this->render('AppBundle:MealPlan:edit.html.twig');
+    }
+
+    /**
+     * @Route("admin/mitybos-programos/delete/{id}", name="meal-plan-delete")
+     */
+    public function mealDeleteAction($id)
+    {
+        return $this->render('AppBundle:MealPlan:delete.html.twig');
+    }
+
 }
--- src/AppBundle/Controller/SportPlanController.php
+++ src/AppBundle/Controller/SportPlanController.php
@@ -17,4 +17,37 @@ class SportPlanController extends Controller
         return $this->render('AppBundle:SportPlan:index.html.twig');
     }
 
+
+    /**
+     * @Route("admin/sporto-programos/list", name="sport-plan-list")
+     */
+    public function sportListAction()
+    {
+        return $this->render('AppBundle:SportPlan:list.html.twig');
+    }
+
+    /**
+     * @Route("admin/sporto-programos/show/{id}", name="sport-plan-show")
+     */
+    public function sportShowAction($id)
+    {
+        return $this->render('AppBundle:SportPlan:show.html.twig');
+    }
+
+    /**
+     * @Route("admin/sporto-programos/edit/{id}", name="sport-plan-edit")
+     */
+    public function sportEditAction($id)
+    {
+        return $this->render('AppBundle:SportPlan:edit.html.twig');
+    }
+
+    /**
+     * @Route("admin/sporto-programos/delete/{id}", name="sport-plan-delete")
+     */
+    public function sportDeleteAction($id)
+    {
+        return $this->render('AppBundle:SportPlan:delete.html.twig');
+    }
+
 }
