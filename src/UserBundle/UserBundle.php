d51cfa9b1cd79cc4df260c80b131d99a33e69363
Aidas Eglinskas <programeriss@gmail.com>
2017-04-18 18:52:30 +0300

    FOSUserBundle

---
+++ src/UserBundle/UserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FOS\UserBundle;
+
+use Sensio\Bundle\GeneratorBundle\Model\Bundle;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Response;
+
+class UserBundle extends Bundle
+{
+
+}
