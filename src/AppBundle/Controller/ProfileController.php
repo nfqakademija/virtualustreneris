86d70dbb8dda6a1177f67fdf6e282fd003f25770
Vilius Norbutas <norbutasvilius@gmail.com>
2017-05-08 15:47:40 +0300

    Profile Controller added

---
+++ src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vilius
+ * Date: 17.5.8
+ * Time: 13.11
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Response;
+
+class ProfileController extends Controller
+{
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+
+    public function profileAction()
+    {
+        return $this->render('AppBundle:Profile:main.html.twig');
+    }
+
+}
