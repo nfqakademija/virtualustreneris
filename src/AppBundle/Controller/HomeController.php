25ed1ec9b2c6e405cc8246225300d7fcd5e90020
Vilius Norbutas <norbutasvilius@gmail.com>
2017-05-08 15:48:59 +0300

    GetCurrentUser service created

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends Controller
 {
@@ -14,7 +15,22 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
+
+
+        $session = new Session();
+        $gender = $session->get('gender');
+        $sportGender = $session->get('sport-gender');
+
+
+        if(!is_null($gender) || !is_null($sportGender)) {
+           $this->addFlash('sessionMessage', 'Jūs jau turite profilyje išsaugoję programų.');
+        }
+
+       $user = $this->get('app.current_user_service')->getUser();
+
         return $this->render('AppBundle:Home:index.html.twig');
     }
+
+
     
 }
