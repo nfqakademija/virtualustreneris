c4bdc91595cbc8ba7cf2e04bb4fe56973388c846
Aidas Eglinskas <programeriss@gmail.com>
2017-05-25 12:24:25 +0300

    Fixed controller formBuilder()

--- src/AppBundle/Controller/MealPlanController.php
+++ src/AppBundle/Controller/MealPlanController.php
@@ -202,141 +202,7 @@ class MealPlanController extends Controller
     public function mealPlanSearchAction()
     {
 
-        $form = $this->createFormBuilder()
-            ->add(
-                'gender',
-                EntityType::class,
-                [
-                'placeholder' => 'Pasirinkite lytį',
-                'class' => Gender::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('gender')
-                        ->OrderBy('gender.gender', 'ASC');
-                },
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                    )
-                ]
-            )
-            ->add(
-                'height',
-                IntegerType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'form-control',
-                    'placeholder' => 'Įveskite savo ūgį (cm)',
-                    'min' => '30',
-                    'max' => '220'
-                )
-                ]
-            )
-            ->add(
-                'weight',
-                IntegerType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'form-control',
-                    'placeholder' => 'Įveskite savo svorį (kg)',
-                    'min' => '30',
-                    'max' => '150'
-                )
-                ]
-            )
-            ->add(
-                'age',
-                IntegerType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'form-control',
-                    'placeholder' => 'Jūsų amžius',
-                    'min' => '4',
-                    'max' => '80'
-                )
-                ]
-            )
-            ->add(
-                'goals',
-                EntityType::class,
-                [
-                'placeholder' => 'Pasirinkite tikslą',
-                'class' => Goals::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('goals')
-                        ->orderBy('goals.title', 'ASC');
-                },
-                'attr'   =>  array(
-                    'class'   => 'form-control')
-                ]
-            )
-            ->add(
-                'activity',
-                EntityType::class,
-                [
-                'placeholder' => 'Jusu fizinis atkyvumas',
-                'class' => Activity::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('activity')
-                        ->orderBy('activity.id', 'ASC');
-                },
-                'attr'   =>  array(
-                    'class'   => 'form-control box-footer')
-                ]
-            )
-            ->add(
-                'varske',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu varškės',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'mesa',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu mėsos',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'grudai',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu grudų',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'zuvis',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu žuvies',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'Ieškoti',
-                SubmitType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'btn btn-special',
-                    'style' => 'color: black;',
-                ),
-                ]
-            )
-            ->getForm();
+        $form = $this->createFormBuilderForMealPlan();
 
 
         return $this->render(
@@ -347,6 +213,145 @@ class MealPlanController extends Controller
         );
     }
 
+    ///Forma
+    private function createFormBuilderForMealPlan(){
+        $form = $this->createFormBuilder()
+            ->add(
+                'gender',
+                EntityType::class,
+                [
+                    'placeholder' => 'Pasirinkite lytį',
+                    'class' => Gender::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('gender')
+                            ->OrderBy('gender.gender', 'ASC');
+                    },
+                    'attr'   =>  array(
+                        'class'   => 'form-control'
+                    )
+                ]
+            )
+            ->add(
+                'height',
+                IntegerType::class,
+                [
+                    'attr'   =>  array(
+                        'class'   => 'form-control',
+                        'placeholder' => 'Įveskite savo ūgį (cm)',
+                        'min' => '30',
+                        'max' => '220'
+                    )
+                ]
+            )
+            ->add(
+                'weight',
+                IntegerType::class,
+                [
+                    'attr'   =>  array(
+                        'class'   => 'form-control',
+                        'placeholder' => 'Įveskite savo svorį (kg)',
+                        'min' => '30',
+                        'max' => '150'
+                    )
+                ]
+            )
+            ->add(
+                'age',
+                IntegerType::class,
+                [
+                    'attr'   =>  array(
+                        'class'   => 'form-control',
+                        'placeholder' => 'Jūsų amžius',
+                        'min' => '4',
+                        'max' => '80'
+                    )
+                ]
+            )
+            ->add(
+                'goals',
+                EntityType::class,
+                [
+                    'placeholder' => 'Pasirinkite tikslą',
+                    'class' => Goals::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('goals')
+                            ->orderBy('goals.title', 'ASC');
+                    },
+                    'attr'   =>  array(
+                        'class'   => 'form-control')
+                ]
+            )
+            ->add(
+                'activity',
+                EntityType::class,
+                [
+                    'placeholder' => 'Jusu fizinis atkyvumas',
+                    'class' => Activity::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('activity')
+                            ->orderBy('activity.id', 'ASC');
+                    },
+                    'attr'   =>  array(
+                        'class'   => 'form-control box-footer')
+                ]
+            )
+            ->add(
+                'varske',
+                CheckboxType::class,
+                [
+                    'label'    => 'Nemėgstu varškės',
+                    'attr'   =>  array(
+                        'class'   => 'form-control'
+                    ),
+                    'required' => false
+                ]
+            )
+            ->add(
+                'mesa',
+                CheckboxType::class,
+                [
+                    'label'    => 'Nemėgstu mėsos',
+                    'attr'   =>  array(
+                        'class'   => 'form-control'
+                    ),
+                    'required' => false
+                ]
+            )
+            ->add(
+                'grudai',
+                CheckboxType::class,
+                [
+                    'label'    => 'Nemėgstu grudų',
+                    'attr'   =>  array(
+                        'class'   => 'form-control'
+                    ),
+                    'required' => false
+                ]
+            )
+            ->add(
+                'zuvis',
+                CheckboxType::class,
+                [
+                    'label'    => 'Nemėgstu žuvies',
+                    'attr'   =>  array(
+                        'class'   => 'form-control'
+                    ),
+                    'required' => false
+                ]
+            )
+            ->add(
+                'Susidaryk',
+                SubmitType::class,
+                [
+                    'attr'   =>  array(
+                        'class'   => 'btn btn-special',
+                        'style' => 'padding-top: 10px; color: black;',
+                    ),
+                ]
+            )
+            ->getForm();
+        return $form;
+    }
 
     /**
      * @Route("/meal-plan/results", name="meal_plan_result")
@@ -436,141 +441,7 @@ class MealPlanController extends Controller
             $age_error = true;
         }
 
-        $form = $this->createFormBuilder()
-            ->add(
-                'gender',
-                EntityType::class,
-                [
-                'placeholder' => 'Pasirinkite lytį',
-                'class' => Gender::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('gender')
-                        ->OrderBy('gender.gender', 'ASC');
-                },
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                )
-                ]
-            )
-            ->add(
-                'height',
-                IntegerType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'form-control',
-                    'placeholder' => 'Įveskite savo ūgį (cm)',
-                    'min' => '30',
-                    'max' => '220'
-                )
-                ]
-            )
-            ->add(
-                'weight',
-                IntegerType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'form-control',
-                    'placeholder' => 'Įveskite savo svorį (kg)',
-                    'min' => '30',
-                    'max' => '150'
-                )
-                ]
-            )
-            ->add(
-                'age',
-                IntegerType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'form-control',
-                    'placeholder' => 'Jūsų amžius',
-                    'min' => '4',
-                    'max' => '80'
-                )
-                ]
-            )
-            ->add(
-                'goals',
-                EntityType::class,
-                [
-                'placeholder' => 'Pasirinkite tikslą',
-                'class' => Goals::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('goals')
-                        ->orderBy('goals.title', 'ASC');
-                },
-                'attr'   =>  array(
-                    'class'   => 'form-control')
-                ]
-            )
-            ->add(
-                'activity',
-                EntityType::class,
-                [
-                'placeholder' => 'Jusu fizinis atkyvumas',
-                'class' => Activity::class,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('activity')
-                        ->orderBy('activity.id', 'ASC');
-                },
-                'attr'   =>  array(
-                    'class'   => 'form-control box-footer')
-                ]
-            )
-            ->add(
-                'varske',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu varškės',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'mesa',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu mėsos',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'grudai',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu grudų',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'zuvis',
-                CheckboxType::class,
-                [
-                'label'    => 'Nemėgstu žuvies',
-                'attr'   =>  array(
-                    'class'   => 'form-control'
-                ),
-                'required' => false
-                ]
-            )
-            ->add(
-                'Ieškoti',
-                SubmitType::class,
-                [
-                'attr'   =>  array(
-                    'class'   => 'btn btn-special',
-                    'style' => 'color: black;',
-                ),
-                ]
-            )
-            ->getForm();
+        $form = $this->createFormBuilderForMealPlan();
 
         if ($error) {
             return $this->render(
